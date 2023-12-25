00a8e493bf04c3992b4dda5dd333c82d52047a60
Enes <69612461+enessakircolak@users.noreply.github.com>
2023-12-25 17:45:00 +0300

    Add files via upload

---
+++ ImpossibleDisassembly/Impossibledisasm.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+
+void naber() {
+    __asm {
+        _emit 0xE8
+    }
+    std::cout << "selam canim";
+
+}
+
+
+int main()
+{
+    __asm {
+        _emit 0xEB
+        _emit 0xFF
+        _emit 0xC0
+    };
+
+   
+    __asm {
+        call naber+4
+    }
+   
+    std::cout << "ZAYOTEM";
+
+
+
+}
